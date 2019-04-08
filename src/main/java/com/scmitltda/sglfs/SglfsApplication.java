d2075bb8e30cb98317113dad9feb508118df6da2
Marco Molinaro <molinaromarcoaurelio@gmail.com>
2019-04-08 16:16:30 -0300

    Created Project

---
+++ src/main/java/com/scmitltda/sglfs/SglfsApplication.java
@@ -0,0 +1,13 @@
+package com.scmitltda.sglfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SglfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SglfsApplication.class, args);
+	}
+
+}
