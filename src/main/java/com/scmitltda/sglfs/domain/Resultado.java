b518b11bba0dc219580a5ed7fa7b2ddf6992eb4d
Marco Molinaro <molinaromarcoaurelio@gmail.com>
2019-04-08 17:52:32 -0300

    Created Rest findAll() for Resultado.

---
+++ src/main/java/com/scmitltda/sglfs/domain/Resultado.java
@@ -0,0 +1,114 @@
+package com.scmitltda.sglfs.domain;
+
+import java.io.Serializable;
+
+public class Resultado implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String numero;
+	private String data;
+	private Boolean acumulado;
+	private Double valorAcumulado;
+	private Double proximoEstimativa;
+	private String proximoData;
+	
+	public Resultado() {}
+	
+	public Resultado(String id, String numero, String data, Boolean acumulado, Double valorAcumulado, Double proximoEstimativa,
+			String proximoData) {
+		super();
+		this.id = id;
+		this.numero = numero;
+		this.data = data;
+		this.acumulado = acumulado;
+		this.valorAcumulado = valorAcumulado;
+		this.proximoEstimativa = proximoEstimativa;
+		this.proximoData = proximoData;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public Boolean getAcumulado() {
+		return acumulado;
+	}
+
+	public void setAcumulado(Boolean acumulado) {
+		this.acumulado = acumulado;
+	}
+
+	public Double getValorAcumulado() {
+		return valorAcumulado;
+	}
+
+	public void setValorAcumulado(Double valorAcumulado) {
+		this.valorAcumulado = valorAcumulado;
+	}
+
+	public Double getProximoEstimativa() {
+		return proximoEstimativa;
+	}
+
+	public void setProximoEstimativa(Double proximoEstimativa) {
+		this.proximoEstimativa = proximoEstimativa;
+	}
+
+	public String getProximoData() {
+		return proximoData;
+	}
+
+	public void setProximoData(String proximoData) {
+		this.proximoData = proximoData;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Resultado other = (Resultado) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
--- src/main/java/com/scmitltda/sglfs/resourses/ResultadoResource.java
+++ src/main/java/com/scmitltda/sglfs/resourses/ResultadoResource.java
@@ -1,16 +1,33 @@
 package com.scmitltda.sglfs.resourses;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.scmitltda.sglfs.domain.Resultado;
 
 @RestController
 @RequestMapping(value="/resultados")
 public class ResultadoResource {
 	
 	@GetMapping
-	public String listar() {
-		return "REST está funcionando";
+	public ResponseEntity<List<Resultado>> findAll() {
+		
+		Resultado r1 = new Resultado("1", "1", "08-04-2019",false, 0.0, 1000000.00, "10-04-2019");
+		Resultado r2 = new Resultado("1", "2", "10-04-2019",true, 1000000.30, 2000000.30, "12-04-2019");
+		Resultado r3 = new Resultado("1", "3", "12-04-2019",false, 0.0, 1000000.00, "14-04-2019");
+		
+		List<Resultado> resultados = new ArrayList<Resultado>();
+		resultados.addAll(Arrays.asList(r1, r2, r3));
+		
+		return ResponseEntity.ok().body(resultados);
 	}
+	
+	
 
 }
